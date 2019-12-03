3c77c104a5a2647b61fa6c2c3bef87e572184dd3
Iulia Bucur <bucur.iulia97@gmail.com>
2019-12-03 09:14:13 +0200

    Changed the code according to comments.

--- GuessTheNumber/GuessTheNumber/Program.cs
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GuessTheNumber
 {
@@ -10,32 +6,39 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+            int numberOfTries = 0;
+
             Random random = new Random();
             var number = random.Next(0, 100);
-            int numberOfTries = 0;
-            Console.WriteLine("A random number has been generated. Can you guess it?");
-            int input = 0;
-
-            while (input != number)
+            Console.Write("A random number has been generated. Can you guess it? Your guess: ");
+            int input;
+            do
             {
-                Console.Write("Your guess: ");
-                int.TryParse(Console.ReadLine(), out input);
-                if (input > number)
+                if (int.TryParse(Console.ReadLine(), out input))
                 {
-                    Console.Write("Too high! Try again. Your guess: ");
-                    numberOfTries++;
-                }
-                else if (input < number)
-                {
-                    Console.Write("Too low! Try again. Your guess: ");
-                    numberOfTries++;
+                    if (input > number)
+                    {
+                        Console.Write("Too high! Try again. Your guess: ");
+                        numberOfTries++;
+                    }
+                    else if (input < number)
+                    {
+                        Console.Write("Too low! Try again. Your guess: ");
+                        numberOfTries++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input} is right! Congratulations. Number of attempts: {++numberOfTries}");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"{input} is right! Congratulations. Number of attempts: {++numberOfTries}");
-                } 
-                
-            }
+                    Console.WriteLine("Your guess must be a number!");
+                    numberOfTries++;
+                }
+            } while (input != number);
+
+            Console.WriteLine("Press any key to end the program..");
             Console.ReadLine();
         }
     }
